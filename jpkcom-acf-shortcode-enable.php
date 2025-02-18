cee7d4f7d0dbec7cc2a96b9a4da350d279ac1aeb
Jean Pierre Kolb <jp.kolb@creationell.de>
2025-02-18 17:09:19 +0100

    v1.0.2

--- jpkcom-acf-shortcode-enable.php
+++ jpkcom-acf-shortcode-enable.php
@@ -1,11 +1,16 @@
 <?php
 /*
 Plugin Name: JPKCom ACF (Pro) Enable Shortcode Plugin
+Plugin URI: https://www.jpkc.com/
 Description: Shortcodes can be used within a WYGIWYG to display another field’s value.
-Version: 1.0.1
+Version: 1.0.2
 Author: Jean Pierre Kolb <jpk@jpkc.com>
 Author URI: https://www.jpkc.com
 Requires Plugins: advanced-custom-fields-pro
+Requires at least: 6.7
+Requires PHP: 8.3
+License: MIT
+License URI: https://opensource.org/license/MIT
 GitHub Plugin URI: JPKCom/jpkcom-acf-shortcode-enable
 Primary Branch: main
 */
