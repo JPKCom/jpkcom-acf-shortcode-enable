641c55f4b596972df50ab9626a6215e3a38e11ad
Jean Pierre Kolb <jpk@jpkc.com>
2026-07-29 09:30:27 +0200

    v2.0.7: tested up to WordPress 7.1, minimum 6.9 throughout
    - Plugin header and README now declare Tested up to: 7.1
    - The bundled updater bailed out below WordPress 6.8 while the plugin header has
      required 6.9 for several releases, so the check could never fire on a supported
      installation. Both now say 6.9.
    - Release manifest fallbacks for requires/tested raised to 6.9 / 7.1

--- jpkcom-acf-shortcode-enable.php
+++ jpkcom-acf-shortcode-enable.php
@@ -3,16 +3,16 @@
 Plugin Name: JPKCom ACF (Pro) Enable Shortcode
 Plugin URI: https://github.com/JPKCom/jpkcom-acf-shortcode-enable
 Description: Shortcodes can be used within a WYSIWYG to display another field’s value.
-Version: 2.0.6
+Version: 2.0.7
 Author: Jean Pierre Kolb <jpk@jpkc.com>
 Author URI: https://www.jpkc.com
 Contributors: JPKCom
 Tags: ACF, Shortcode, HTML, Gutenberg
 Requires Plugins: advanced-custom-fields-pro
 Requires at least: 6.9
-Tested up to: 7.0
+Tested up to: 7.1
 Requires PHP: 8.3
-Stable tag: 2.0.6
+Stable tag: 2.0.7
 License: GPL-2.0-or-later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 */
@@ -30,7 +30,7 @@ if ( ! defined( constant_name: 'WPINC' ) ) {
  * @since 2.0.3
  */
 if ( ! defined( 'JPKCOM_ACF_SHORTCODE_ENABLE_VERSION' ) ) {
-    define( 'JPKCOM_ACF_SHORTCODE_ENABLE_VERSION', '2.0.6' );
+    define( 'JPKCOM_ACF_SHORTCODE_ENABLE_VERSION', '2.0.7' );
 }
 
 
